200322444e629ab7efd32412977f648caed5599e
keshavsoft <nknnkumar@yahoo.com>
2026-05-22 09:58:18 +0530

    usage

--- bin/v5/core/showUsage.js
+++ bin/v5/core/showUsage.js
@@ -1,41 +1,41 @@
 import allVersions from "./getAllVersions.js";
 
-/*
-KSchema CLI – Dynamic Usage Renderer
-*/
+const g = "\x1b[32m";
+const y = "\x1b[33m";
+const c = "\x1b[36m";
+const gray = "\x1b[90m";
+const b = "\x1b[34m";
+const r = "\x1b[0m";
+
+const commands = () =>
+    Object.keys(allVersions)
+        .map(name => `  ${g}${name}${r}`)
+        .join("\n");
+
+const examples = () =>
+    Object.keys(allVersions)
+        .map(name => `  ${gray}npx @keshavsoft-org/express-todo ${name}${r}`)
+        .join("\n");
+
+const docs = () => `
+${y}Docs:${r}
+  ${b}https://keshavsoft.github.io/express-todo/${r}
+`;
 
 export default function showUsage(version) {
-    const g = "\x1b[32m";
-    const y = "\x1b[33m";
-    const c = "\x1b[36m";
-    const gray = "\x1b[90m";
-    const r = "\x1b[0m";
-
-    const commandLines = Object.entries(allVersions)
-        .map(([name]) =>
-            `  ${g}${name.padEnd(20)}${r}`
-        )
-        .join("\n");
-
-    const exampleLines = Object.entries(allVersions)
-        .map(([name]) =>
-            `  ${gray}npx @keshavsoft/express-todo ${name}${r}`
-        )
-        .join("\n");
 
     console.log(`
 ${c}🚀 express-todo v${version}${r}
 
 ${y}Usage:${r}
-  ${g}npx @keshavsoft/express-todo${r} <command> [options]
+  ${g}npx @keshavsoft-org/express-todo${r} <command>
 
 ${y}Commands:${r}
-${commandLines}
+${commands()}
 
 ${y}Examples:${r}
-${exampleLines}
+${examples()}
 
-${y}Tip:${r}
-  ${gray}npm i -g @keshavsoft/express-todo${r}
+${docs()}
 `);
 }
