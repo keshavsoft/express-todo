474dc1e8348b6c3487788b04eda282557208d6d8
keshavsoft <nknnkumar@yahoo.com>
2026-05-23 11:21:11 +0530

    still work

--- bin/v7/commands/simple.js
+++ bin/v7/commands/simple.js
@@ -1,19 +1,84 @@
+/*
+bin/v5/commands/simple.js
+*/
+
 import { locateSource } from "./simple/steps/locateSource.js";
 import { locateDestination } from "./simple/steps/locateDestination.js";
 import { createProject } from "./simple/steps/createProject.js";
 import { announce } from "./simple/steps/announce.js";
 
 import resolveFolderName from "../core/resolveFolderName.js";
+/**
+ * @typedef {Object} simpleOptions
+ * @property {string} folderName
+ * @property {boolean} isAnnounce
+ * @property {boolean} showLog
+ */
 
-export default ({ folderName = "" }) => {
+/**
+ * @param {simpleOptions} options
+ */
+export default ({
+    folderName = "",
+    isAnnounce = false,
+    showLog = false,
+    inProcessPath
+}) => {
+
+    if (showLog) {
+        console.log("\n[1] Resolving folder name...");
+    }
+
     const resolvedFolderName = resolveFolderName({
         name: folderName
     });
 
-    const source = locateSource();
-    const destination = locateDestination({ inResolvedFolderName: resolvedFolderName });
+    if (showLog) {
+        console.log("✔ Folder name resolved");
+        console.log(resolvedFolderName);
+    }
 
-    createProject({ source, destination });
+    if (showLog) {
+        console.log("\n[2] Locating source...");
+    }
 
-    announce({ inResolvedFolderName: resolvedFolderName });
+    const source = locateSource({
+        showLog
+    });
+
+    if (showLog) {
+        console.log("\n[3] Locating destination...");
+    }
+
+    const destination = locateDestination({
+        inResolvedFolderName: resolvedFolderName,
+        showLog,
+        inProcessPath
+    });
+
+    if (showLog) {
+        console.log("\n[4] Creating project...");
+    }
+
+    createProject({
+        source,
+        destination,
+        showLog
+    });
+
+    if (isAnnounce) {
+
+        if (showLog) {
+            console.log("\n[5] Announcing...");
+        }
+
+        announce({
+            inResolvedFolderName: resolvedFolderName,
+            showLog
+        });
+    }
+
+    if (showLog) {
+        console.log("\n✔ Command completed\n");
+    };
 };
