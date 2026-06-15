34431687897c3d3137eb772ae9e6e6aa7760707e
keshavsoft <nknnkumar@yahoo.com>
2026-06-15 07:35:18 +0530

    10 st

---
+++ bin/v10/commands/simpleWithUi.js
@@ -0,0 +1,84 @@
+/*
+bin/v5/commands/simpleWithUi.js
+*/
+
+import { locateSource } from "./simpleWithUi/steps/locateSource.js";
+import { locateDestination } from "./simpleWithUi/steps/locateDestination.js";
+import { createProject } from "./simpleWithUi/steps/createProject.js";
+import { announce } from "./simpleWithUi/steps/announce.js";
+
+import resolveFolderName from "../core/resolveFolderName.js";
+/**
+ * @typedef {Object} simpleWithUiOptions
+ * @property {string} folderName
+ * @property {boolean} isAnnounce
+ * @property {boolean} showLog
+ */
+
+/**
+ * @param {simpleWithUiOptions} options
+ */
+export default ({
+    folderName = "",
+    isAnnounce = false,
+    showLog = false,
+    inProcessPath
+}) => {
+
+    if (showLog) {
+        console.log("\n[1] Resolving folder name...");
+    }
+
+    const resolvedFolderName = resolveFolderName({
+        name: folderName
+    });
+
+    if (showLog) {
+        console.log("✔ Folder name resolved");
+        console.log(resolvedFolderName);
+    }
+
+    if (showLog) {
+        console.log("\n[2] Locating source...");
+    }
+
+    const source = locateSource({
+        showLog
+    });
+
+    if (showLog) {
+        console.log("\n[3] Locating destination...");
+    }
+
+    const destination = locateDestination({
+        inResolvedFolderName: resolvedFolderName,
+        showLog,
+        inProcessPath
+    });
+
+    if (showLog) {
+        console.log("\n[4] Creating project...");
+    }
+
+    createProject({
+        source,
+        destination,
+        showLog
+    });
+
+    if (isAnnounce) {
+
+        if (showLog) {
+            console.log("\n[5] Announcing...");
+        }
+
+        announce({
+            inResolvedFolderName: resolvedFolderName,
+            showLog
+        });
+    }
+
+    if (showLog) {
+        console.log("\n✔ Command completed\n");
+    };
+};
